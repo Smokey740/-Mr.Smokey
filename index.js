773c3e9bbe526d39b82ddb00d3279c910453c369
Smokey740 <mrsmokey232@gmail.com>
2025-06-10 08:10:16 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,37 +1,44 @@
 /**
- * Goat Bot Render Deployment Fix by Eren
+ * @author NTKhang
+ * ! The source code is written by NTKhang, please don't change the author's name everywhere. Thank you for using
+ * ! Official source code: https://github.com/ntkhang03/Goat-Bot-V2
+ * ! If you do not download the source code from the above address, you are using an unknown version and at risk of having your account hacked
+ *
+ * English:
+ * ! Please do not change the below code, it is very important for the project.
+ * It is my motivation to maintain and develop the project for free.
+ * ! If you change it, you will be banned forever
+ * Thank you for using
+ *
+ * Vietnamese:
+ * ! Vui lòng không thay đổi mã bên dưới, nó rất quan trọng đối với dự án.
+ * Nó là động lực để tôi duy trì và phát triển dự án miễn phí.
+ * ! Nếu thay đổi nó, bạn sẽ bị cấm vĩnh viễn
+ * Cảm ơn bạn đã sử dụng
  */
 
-const express = require("express");
 const { spawn } = require("child_process");
 const log = require("./logger/log.js");
 
-// === Express server to keep Render service alive ===
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-app.get("/", (req, res) => {
-	res.send("EREN BOT RUNNING \n author: Eren \n Status: smooth 🥵");
-});
-
-app.listen(PORT, () => {
-	console.log(`✅ Server running at http://localhost:${PORT}`);
-});
-
-// === Start the Goat bot process ===
 function startProject() {
-	const child = spawn("node", ["Goat.js"], {
-		cwd: __dirname,
-		stdio: "inherit",
-		shell: true
-	});
+	try {
+		const child = spawn("node", ["Goat.js"], {
+			cwd: __dirname,
+			stdio: "inherit",
+			shell: true
+		});
 
-	child.on("close", (code) => {
-		if (code === 2) {
-			log.info("Restarting Project...");
-			startProject();
-		}
-	});
+		child.on("close", (code) => {
+			if (code === 2) {
+				log.info("Restarting Project...");
+				startProject(); // Recursive call for auto-restart
+			} else if (code !== 0) {
+				log.error(`Process exited with code: ${code}`);
+			}
+		});
+	} catch (err) {
+		log.error("Failed to start project:", err);
+	}
 }
 
 startProject();
